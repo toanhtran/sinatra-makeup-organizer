d53b51eb227ec2d89855698c7930bb39a39528ff
toanhtran <toanh.t.tran@gmail.com>
2016-08-22 15:02:21 +0900

    update user model with slug method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_many  :makeup_bags
   has_secure_password
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find {|user| user.slug == slug}
+  end
+
 end
