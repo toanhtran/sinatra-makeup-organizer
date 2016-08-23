2091bae3af90790c8208b8f7626c7a832d94afa0
toanhtran <toanh.t.tran@gmail.com>
2016-08-23 14:06:01 +0900

    add new lipstick route to lipstick_controller

--- app/controllers/lipsticks_controller.rb
+++ app/controllers/lipsticks_controller.rb
@@ -1,7 +1,14 @@
 class LipsticksController < ApplicationController
 
   get '/lipsticks' do
+    redirect_if_not_logged_in
     @lipsticks = Lipstick.all
     erb :'lipsticks/index'
+  end
+
+  get '/lipsticks/new' do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    erb :'lipsticks/new'
   end
 end
