38077873d07f3f90588db272258951c51f8eaa75
toanhtran <toanh.t.tran@gmail.com>
2016-08-23 10:49:43 +0900

    Materialize framework and update UI

--- app/controllers/makeup_bags_controller.rb
+++ app/controllers/makeup_bags_controller.rb
@@ -1,11 +1,41 @@
 class MakeupBagsController < ApplicationController
 
-  get '/bags' do
-    @bags = MakeupBag.all
-    erb :'makeup_bags/index'
+
+  get "/bags/new" do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    erb :'makeup_bags/new'
   end
 
-  get '/bags/new' do
-    erb :'makeup_bags/new'
+  get "/bags/:id/edit" do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    @bag = MakeupBag.find_by_id(params[:id])
+    erb :'makeup_bags/edit'
+  end
+
+  post "/bags/:id" do
+    redirect_if_not_logged_in
+    @bag = MakeupBag.find(params[:id])
+    unless MakeupBag.valid_params?(params)
+      redirect "/bags/#{@bag.id}/edit?error=invalid bag"
+    end
+    @bag.update(params.select{|k|k=="name"})
+    redirect "/bags/#{@bag.id}"
+  end
+
+  get "/bags/:id" do
+    redirect_if_not_logged_in
+    @bag = MakeupBag.find_by_id(params[:id])
+    erb :'makeup_bags/show'
+  end
+
+  post "/bags" do
+    redirect_if_not_logged_in
+    unless MakeupBag.valid_params?(params)
+      redirect "/bags/new?error=invalid bag"
+    end
+    MakeupBag.create(params)
+    redirect "/bags"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,4 @@ class User < ActiveRecord::Base
   has_many  :makeup_bags
   has_secure_password
 
-  def slug
-    username.downcase.gsub(" ","-")
-  end
-
-  def self.find_by_slug(slug)
-    User.all.find {|user| user.slug == slug}
-  end
-
 end
