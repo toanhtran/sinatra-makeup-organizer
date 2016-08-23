206e903496dd62a12f2c4f5823bb95c651095538
toanhtran <toanh.t.tran@gmail.com>
2016-08-23 19:11:43 +0900

    update user signup form

--- app/controllers/lipsticks_controller.rb
+++ app/controllers/lipsticks_controller.rb
@@ -11,4 +11,36 @@ class LipsticksController < ApplicationController
     @error_message = params[:error]
     erb :'lipsticks/new'
   end
+
+  get '/lipsticks/:id/edit' do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    @lipstick = Lipstick.find_by_id(params[:id])
+    erb :'lipsticks/edit'
+  end
+
+  post '/lipsticks/:id' do
+    redirect_if_not_logged_in
+    @lipstick = Lipstick.find_by_id(params[:id])
+    unless Lipstick.valid_params?(params)
+      redirect '/lipsticks/#{@lipstick.id}/edit?error=invalid lipstick'
+    end
+    @lipstick.update(params.select{|key| k=="shade" || key=="brand" || key=="makeupbag_id"})
+    redirect '/lipsticks/#{@lipstick.id}'
+  end
+
+  get '/lipsticks/:id' do
+    redirect_if_not_logged_in
+    @lipstick = Lipstick.find_by_id(params[:id])
+    erb :'lipsticks/show'
+  end
+
+  post '/lipsticks' do
+    redirect_if_not_logged_in
+    unless Lipstick.valid_params?(params)
+      redirect '/lipsticks/new?error=invalid lipstick'
+    end
+    Lipstick.create(:shade => params["shade"], :brand => params["brand"])
+    redirect '/lipsticks'
+  end
 end
--- app/models/lipstick.rb
+++ app/models/lipstick.rb
@@ -2,7 +2,7 @@ class Lipstick < ActiveRecord::Base
   belongs_to  :makeup_bag
 
   def self.valid_params?(params)
-    return!params[:name].empty? && !params[:brand].empty?
+    return !params[:shade].empty? && !params[:brand].empty?
   end
 
 end
